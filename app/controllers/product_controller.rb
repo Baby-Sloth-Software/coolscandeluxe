b1941fbe36be19edcfdc76564e1cca332a3d1bf9
Joe Winter <joe@joeventures.com>
2016-04-16 13:38:17 -0400

    Add product create controller

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -6,6 +6,17 @@ class ProductController < ApplicationController
   end
 
   def create
+    @product = Product.new(name: params[:name],
+      quantity: params[:quantity].to_i,
+      rvalue: params[:rvalue].to_i,
+      gvalue: params[:gvalue].to_i,
+      bvalue: params[:bvalue].to_i)
+    if @product.save
+      respond_to do |format|
+        format.html
+        format.json
+      end
+    end
   end
 
   def show
