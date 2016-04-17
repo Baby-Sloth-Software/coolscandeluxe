f515b702919604704f89e2a72e33c27e9bdb2240
violetaria <terri.chu@outlook.com>
2016-04-17 10:52:08 -0400

    respond back with customer name

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,8 +1,21 @@
 class TextsController < ApplicationController
 
   def index
+    # Parameters: {"ToCountry"=>"US",
+    #              "ToState"=>"GA",
+    #              "SmsMessageSid"=>"SMecadf0061ffb938491ccfc1164a1e2fc",
+    #              "NumMedia"=>"0", "ToCity"=>"COLUMBUS", "FromZip"=>"32801",
+    #              "SmsSid"=>"SMecadf0061ffb938491ccfc1164a1e2fc",
+    #              "FromState"=>"FL", "SmsStatus"=>"received", "FromCity"=>"ORLANDO", "Body"=>"Hey", "FromCountry"=>"US",
+    #              "To"=>"+16787862528", "ToZip"=>"31901", "NumSegments"=>"1",
+    #              "MessageSid"=>"SMecadf0061ffb938491ccfc1164a1e2fc", "AccountSid"=>"AC389ab8c76fd5d42343da183110d741e8",
+    #              "From"=>"+14072837742", "ApiVersion"=>"2010-04-01"}
+    #
+    phone_number = params['From']
+    customer = Customer.find_by_phone(phone_number)
+
     twiml = Twilio::TwiML::Response.new do |r|
-      r.Message "Hey Monkey. Thanks for the message!"
+      r.Message "Hey #{customer.first_name} #{customer.last_name}. Thanks for the message!"
     end
     render xml: twiml.text
   end
