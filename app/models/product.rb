fcebc42665e3faf7b6f74e23ce7ce13a7afd5f08
Joe Winter <joe@joeventures.com>
2016-04-17 13:44:52 -0400

    Make find_product_by_range return a string no matter what. So there.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
 
-  def self.find_product_by_range(crgb_hash)
+  def find_product_by_range(crgb_hash)
     green = {c: (3800..4300), r: (800..1200), g: (1900..2100), b: (1000..1300)}
     red = {c: (4300..4800), r: (2500..2900), g: (700..1200), b: (700..1200)}
     blue = {c: (7900..8400), r: (1200..1600), g: (2800..3200), b: (3400..4200)}
@@ -8,6 +8,7 @@ class Product < ActiveRecord::Base
     rcolor = 'green' if green[:c] === crgb_hash[:c_value] && green[:r] === crgb_hash[:r_value] && green[:g] === crgb_hash[:g_value] && green[:b] === crgb_hash[:b_value]
     rcolor = 'red' if red[:c] === crgb_hash[:c_value] && red[:r] === crgb_hash[:r_value] && red[:g] === crgb_hash[:g_value] && red[:b] === crgb_hash[:b_value]
     rcolor = 'blue' if blue[:c] === crgb_hash[:c_value] && blue[:r] === crgb_hash[:r_value] && blue[:g] === crgb_hash[:g_value] && blue[:b] === crgb_hash[:b_value]
+    rcolor ||= ''
     rcolor
   end
 
