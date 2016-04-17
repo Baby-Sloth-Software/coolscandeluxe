f37f424eae52e6539b9bea3ce4d11de4b9ad8af2
violetaria <terri.chu@outlook.com>
2016-04-17 11:44:03 -0400

    add more state stuff

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -10,17 +10,25 @@ class TextsController < ApplicationController
     #              "To"=>"+16787862528", "ToZip"=>"31901", "NumSegments"=>"1",
     #              "MessageSid"=>"SMecadf0061ffb938491ccfc1164a1e2fc", "AccountSid"=>"AC389ab8c76fd5d42343da183110d741e8",
     #              "From"=>"+14072837742", "ApiVersion"=>"2010-04-01"}
+    enable :sessions
 
     phone_number = params['From']
     customer = Customer.find_by_phone(phone_number)
 
     if customer.start?
-      product = params['Body']
-      message = "Hey #{customer.first_name}!  It's great to hear that you need more #{product}.  Please reply 1 to CONFIRM or 2 to CANCEL."
-      customer.conversation_state = :determine_product
-      customer.conversation_id = params['MessageSid']
+      product = Product.find_by_name(downcase(params['Body']))
+      message = "Hey #{customer.first_name}!  It's great to hear that you need more #{product.name.titleize}.  Please reply 1 to CONFIRM or 2 to CANCEL."
+      session['product'] = product.name.titleize
+      customer.update(conversation_state: Customer.conversation_states[:determine_product], conversation_id: params['MessageSid'])
     elsif customer.determine_product?
-
+      response = params['Body'].to_i
+      if response == 1
+        customer.update(conversation_state: Customer.conversation_states[:product_confirmed], conversation_id: params['MessageSid'])
+        message = "Stock Request for #{session['product']}: Please reply with a quantity."
+      else
+        customer.update(conversation_state: Customer.conversation_states[:start], conversation_id: params['MessageSid'])
+        message = "Stock Request for #{session['product']} Cancelled."
+      end
     elsif customer.product_confirmed?
 
     elsif customer.order_confirmed?
